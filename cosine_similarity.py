40fc2b6efaeb5a7eb6b71d78d219bf8f618d939d
aniket312001 <70058567+aniket312001@users.noreply.github.com>
2020-12-28 13:24:54 +0530

    Add files via upload

---
+++ cosine_similarity.py
@@ -0,0 +1,14 @@
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity 
+
+text = ['London Paris London', 'Paris Paris London']
+
+cv = CountVectorizer()
+
+count_metrix = cv.fit_transform(text)
+
+# print(count_metrix.toarray())   # [[2,1],[1,2]]
+
+similarity_score = cosine_similarity(count_metrix)
+
+print(similarity_score)
